0f224842bce4a7987f4227e1978474554e3a27e3
Oesquimno <wellingtonferreiramelo27@gmail.com>
2025-08-29 00:43:46 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -4,14 +4,13 @@
  * Versão: 3.0 (Com funcionalidade de agendamento por dia da semana)
  */
 
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase, ref, get, set, remove, push, update, onValue } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// ==========================================================================
+// 1. CONFIGURAÇÃO E VARIÁVEIS GLOBAIS
+// ==========================================================================
+
 const firebaseConfig = {
   apiKey: "AIzaSyCFf5gckKE6rg7MFuBYAO84aV-sNrdY2JQ",
   authDomain: "agendamento-esquimo.firebaseapp.com",
@@ -23,10 +22,9 @@ const firebaseConfig = {
   measurementId: "G-Z0EMQ3XQ1D"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-//const analytics = getAnalytics(app);
 
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 
 
 
