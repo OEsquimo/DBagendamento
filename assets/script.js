2c0564bd59ad0109c3a2261d5ad6c75bfaef5854
Oesquimno <wellingtonferreiramelo27@gmail.com>
2025-08-29 00:21:13 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -16,7 +16,7 @@ import { getDatabase, ref, get, set, push } from "https://www.gstatic.com/fireba
 
 // Import the functions you need from the SDKs you need
 //import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
