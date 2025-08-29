92d4ebeb227ea791576353ef643519282439d733
Oesquimno <wellingtonferreiramelo27@gmail.com>
2025-08-28 23:52:33 -0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@
  * Versão: 3.0 (Com funcionalidade de agendamento por dia da semana)
  */
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+//import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getDatabase, ref, get, set, push } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 // ==========================================================================
